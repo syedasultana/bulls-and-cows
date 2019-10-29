f2870f94a6d5d73bc51dfc859956e5b79e10f22c
syeda sultana <syedamahasultana@gmail.com>
2019-10-29 16:12:12 +0000

    added file for InputGuess component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import './App.css';
 
 import RandomNumber from './components/RandomNumber';
+import InputGuess from './components/InputGuess';
 
 function App() {
 
   const [fourDigitNumber, setFourDigitNumber] = React.useState(/*GenerateRandomNo()*/);
-  const [guess, setGuess] = React.useState();
-  const [typedInput, setTypedInput] = React.useState('');
   const [result, setResult] = React.useState('');
   const [warnMessage, setWarnMessage] = React.useState('')
 
@@ -19,16 +18,13 @@ function App() {
         setFourDigitNumber={setFourDigitNumber}   
       /> 
 
-      <input
-        onChange={event => setTypedInput(event.target.value)}
-        placeholder="guess 4 digit number"
+      <InputGuess
+        
+        setWarnMessage={setWarnMessage}
+        setResult={setResult}
+        fourDigitNumber={fourDigitNumber}
       />
-      <button onClick={() => {
-        setGuess(typedInput);
-        //console.log(typedInput);
-        setWarnMessage(CheckForValidGuess(typedInput));
-        setResult(CompareGuess(fourDigitNumber, typedInput));
-      }}>Submit</button>
+      
 
       <h3>{result}</h3>
       <p>{warnMessage}</p>
@@ -40,49 +36,9 @@ function App() {
 
 
 
-function CompareGuess(solution, guess) {
-  //cows
-  //convert guess into an array of digits
-  const guessArrayed = guess.toString().split('');
-  const solutionArrayed = solution.toString().split('');
-  let cows = 0;
-  let bulls = 0;
-  //console.log(guessArrayed);
-  //for each element, check if this element is somewhere in the solutionArrayed
-  for (let x = 0; x < guessArrayed.length; x++) {
-    if (solutionArrayed.includes(guessArrayed[x])) {
-      if (guessArrayed[x] == solutionArrayed[x]) {
-        bulls = bulls + 1;
-      } else {
-        cows = cows + 1;
-      }
-    }
-  }
-  // console.log('cows: ' + cows);
-  // console.log('bulls: ' + bulls);
-
-    return `${bulls} bulls and ${cows} cows`;
 
 
-}
 
-function CheckForValidGuess(userInput) {
-  let digits = /[0-9]/g //regex allows match function to look for 0-9 characters globally
-  let message
-  console.log(userInput.match(digits))
-  if(userInput.length != 4){
-    message = 'Make sure that your guess is 4 digits long'
-  } else if ((userInput.match(digits) == null) || (userInput.match(digits).length != userInput.length)){
-
-    //if the number of characters thats are digits in userInput  is not equal to the original string length, there are characters that are not digits
-    message = 'Make sure that your guess uses numbers 0-9 only'
-
-  } else {
-    message = ''
-  }
-
-  return message
-}
 
 
 export default App;
---
+++ src/components/inputGuess.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function InputGuess({ 
+    setWarnMessage,
+    setResult,
+    fourDigitNumber
+}){
+
+    const [guess, setGuess] = React.useState();
+    const [typedInput, setTypedInput] = React.useState('');
+
+    return (
+        <>
+            <input
+                onChange={event => setTypedInput(event.target.value)}
+                placeholder="guess 4 digit number"
+                />
+            <button onClick={() => {
+                setGuess(typedInput);
+                //console.log(typedInput);
+                setWarnMessage(CheckForValidGuess(typedInput));
+                setResult(CompareGuess(fourDigitNumber, typedInput));
+            }}>Submit</button>
+        </>
+    )
+}
+
+function CheckForValidGuess(userInput) {
+    let digits = /[0-9]/g //regex allows match function to look for 0-9 characters globally
+    let message
+    console.log(userInput.match(digits))
+    if(userInput.length != 4){
+      message = 'Make sure that your guess is 4 digits long'
+    } else if ((userInput.match(digits) == null) || (userInput.match(digits).length != userInput.length)){
+  
+      //if the number of characters thats are digits in userInput  is not equal to the original string length, there are characters that are not digits
+      message = 'Make sure that your guess uses numbers 0-9 only'
+  
+    } else {
+      message = ''
+    }
+  
+    return message
+}
+
+function CompareGuess(solution, guess) {
+    //cows
+    //convert guess into an array of digits
+    const guessArrayed = guess.toString().split('');
+    const solutionArrayed = solution.toString().split('');
+    let cows = 0;
+    let bulls = 0;
+    //console.log(guessArrayed);
+    //for each element, check if this element is somewhere in the solutionArrayed
+    for (let x = 0; x < guessArrayed.length; x++) {
+      if (solutionArrayed.includes(guessArrayed[x])) {
+        if (guessArrayed[x] == solutionArrayed[x]) {
+          bulls = bulls + 1;
+        } else {
+          cows = cows + 1;
+        }
+      }
+    }
+    // console.log('cows: ' + cows);
+    // console.log('bulls: ' + bulls);
+  
+      return `${bulls} bulls and ${cows} cows`;
+  
+  
+}
+
+
+
+export default InputGuess;
